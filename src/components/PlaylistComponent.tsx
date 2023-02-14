8049c104db68bd69f9d9397661e00d6f7e01763d
Yang⭐ <78190786+313yang@users.noreply.github.com>
2023-02-14 21:00:59 +0900

    Update PlaylistComponent.tsx

--- src/components/PlaylistComponent.tsx
+++ src/components/PlaylistComponent.tsx
@@ -17,7 +17,7 @@ export default function PlaylistComponent({ type, keywords }: Props) {
     searchPlaylistKeyword(selected)
   );
 
-  if (error) return <div>An error has occurred: {error?.message}</div>;
+  if (error) return <div>An error has occurred</div>;
   return (
     <PlaylistContainer>
       <h1>{type.title}</h1>
