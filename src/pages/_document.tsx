f2db9dfc4aae653d5298726bdb920c41649c096c
Yang⭐ <78190786+313yang@users.noreply.github.com>
2023-03-10 14:47:21 +0900

    feat: added naver-search-advisor & ga

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Script from "next/script";
 import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
@@ -38,7 +39,19 @@ class MyDocument extends Document {
           <meta name="Keywords" content="playlist, music, spotify, youtube" />
           <meta name="image" content="/favicon.ico" />
           <link rel="icon" href="/favicon.ico" />
+          <meta name="naver-site-verification" content="ba02d1f241b85b328285fa858eec8d21732f186a" />
         </Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1LD7BTF3V9"></Script>
+        <Script
+          id="gtag-init"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1LD7BTF3V9');`,
+          }}
+        ></Script>
         <body>
           <Main />
           <NextScript />
